a1fa83101c6898358635173551a48d8f46ccf822
Rico Qi <qirico@outlook.com>
2025-10-20 12:15:01 -0700

    config: oct20_antdir_circle_inftasks_numiter9k_goal_random_02.

--- c.sh
+++ c.sh
@@ -1,3 +1,3 @@
-python ./dtrain.py -j oct19_antdir_circle_left_right_up_down_inftasks_numiter9k_2 --account stf --qos gpu-2080ti -sH 24 \
+python ./dtrain.py -j oct20_antdir_circle_inftasks_numiter9k_goal_random_02 --account weirdlab --qos gpu-l40 -sH 24 --mem 16 \
 -- python policies/main.py \
-    --cfg configs/meta/ant_dir/circle_left_right_up_down_inftasks_numiter9k.yml \
+    --cfg configs/meta/ant_dir/circle_inftasks_numiter9k_goal_random_02.yml \
