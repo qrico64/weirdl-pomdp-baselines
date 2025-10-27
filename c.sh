1fdfcf5a9855b6bc66d40e41f8491bc16335c87f
Rico Qi <qirico@outlook.com>
2025-10-27 14:51:04 -0700

    config: oct27_nonparallel_antdir_circle_down_quarter_norm_16tasks_goal_relative_normal_02 .

--- c.sh
+++ c.sh
@@ -1,3 +1,3 @@
-python ./dtrain.py -j oct27_nonparallel_antdir_circle_down_quarter_norm_5tasks_goal --account stf --qos gpu-l40 -sH 24 --mem 80 --cpus 20 \
+python ./dtrain.py -j oct27_nonparallel_antdir_circle_down_quarter_norm_16tasks_goal_relative_normal_02 --account stf --qos ckpt -sH 24 --mem 128 --cpus 12 \
 -- python policies/main.py \
     --cfg flexible.yaml \
