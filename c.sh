f1ec1ce1adf66fbbfe4955453e9b7aca2b25f7b0
Rico Qi <qirico@outlook.com>
2025-10-29 01:08:57 -0700

    config: oct29_nominalinevals_antdir_circle_down_quarter_norm_16tasks_goal_nominal .

--- c.sh
+++ c.sh
@@ -1,3 +1,3 @@
-python ./dtrain.py -j oct27_nonparallel_antdir_circle_down_quarter_norm_16tasks_goal_relative_normal_02 --account stf --qos ckpt -sH 24 --mem 128 --cpus 12 \
+python ./dtrain.py -j oct29_nominalinevals_antdir_circle_down_quarter_norm_16tasks_goal_nominal --account weirdlab --qos gpu-l40s -sH 24 --mem 256 --cpus 19 \
 -- python policies/main.py \
     --cfg flexible.yaml \
