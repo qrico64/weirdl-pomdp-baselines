7f55fe88ceeacdafb22ca4c39ae094dd4054fef0
Rico Qi <qirico@outlook.com>
2025-11-09 18:06:27 -0800

    config: nov9_transformer_4layers_48emb_lrx3_antdir_circle_norm_inftasks_goal_1.

--- c.sh
+++ c.sh
@@ -1,6 +1,6 @@
-for seed in 41; do
-    job_name="nov9_fixedtype_attempt6_$((seed - 39))"
-    python ./dtrain.py -j "$job_name" --account weirdlab --qos gpu-a40 -sH 24 --mem 160 --cpus 11 \
+for seed in 40; do
+    job_name="nov9_transformer_4layers_48emb_antdir_circle_norm_inftasks_goal_$((seed - 39))"
+    python ./dtrain.py -j "$job_name" --account cse --qos gpu-l40s -sH 24 --mem 200 --cpus 30 \
     -- python policies/main.py \
         --cfg flexible.yaml \
         --seed "$seed"
