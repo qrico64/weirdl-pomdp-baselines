fdce363ae38e1b896a5de218984532c7df2a61ab
Rico Qi <qirico@outlook.com>
2025-12-05 09:21:37 -0800

    config: Confirmed best AntGoal config for circle.

--- c.sh
+++ c.sh
@@ -1,5 +1,5 @@
-for seed in 42; do
-    job_name="dec4_antgoalposlinear_circle_3layers_combined_reward_x1_$((seed - 39))"
+for seed in 43 44; do
+    job_name="dec5_antgoalposlinear_circle_3layers_combined_reward_x2_$((seed - 39))"
     python ./dtrain.py -j "$job_name" --account stf --qos gpu-2080ti -sH 24 --mem 45 --cpus 5 \
     -- python policies/main.py \
         --cfg flexible.yaml \
