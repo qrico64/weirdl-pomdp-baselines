f6b8b49f425a00bb6237596160ae5d542f89ad0f
Rico Qi <qirico@outlook.com>
2025-12-23 05:04:36 -0800

    config: Recent comparison config.

--- scripts/plot_comparison.py
+++ scripts/plot_comparison.py
@@ -9,13 +9,20 @@ if __name__ == "__main__":
         for typ2 in ["return", "success"]:
             column = f"metrics/{typ2}_{typ}_total" if typ2 == "return" else f"metrics/{typ2}_rate_{typ}"
             try:
-                plot_comparison(f"viz/dec6/dec6_antgoalposlinear_circle_reward_x2__{typ2}_{typ}.png", [
-                    ["experiments/dec4/dec4_antgoalposlinear_circle_3layers_combined_reward_x2_3",
-                    "experiments/dec5/dec5_antgoalposlinear_circle_3layers_combined_reward_x2_4",
-                    "experiments/dec5/dec5_antgoalposlinear_circle_3layers_combined_reward_x2_5"],
+                plot_comparison(f"viz/dec23/dec23_hypothesis1_version2_circle_1_2__{typ2}_{typ}.png", [
+                    ["experiments/dec21/dec21_hypothesis1_markov_circle_1_2_goal_3",
+                    "experiments/dec21/dec21_hypothesis1_markov_circle_1_2_goal_4",
+                    "experiments/dec21/dec21_hypothesis1_markov_circle_1_2_goal_5"],
+                    ["experiments/dec21/dec21_hypothesis1_nonmarkov_circle_1_2_goal_3",
+                    "experiments/dec21/dec21_hypothesis1_nonmarkov_circle_1_2_goal_4",
+                    "experiments/dec21/dec21_hypothesis1_nonmarkov_circle_1_2_goal_5"],
+                    ["experiments/dec21/dec21_hypothesis1_nonmarkov_circle_1_2_nogoal_3",
+                    "experiments/dec21/dec21_hypothesis1_nonmarkov_circle_1_2_nogoal_5"],
                 ], column=column, labels=[
-                    "reward x2",
-                ], title=f"antgoal linear circle ({typ})")
+                    "markov with goal",
+                    "non-markov with goal and rewards",
+                    "non-markov with rewards",
+                ], title=f"antgoal w/ train radius 1 & test radius 2 ({typ})")
             except AssertionError as e:
                 raise
             except Exception:
